e0f956077a83723215492859ca7175fdce308ec4
Santiago Quipildor <quipildorsantiago@gmail.com>
2023-09-02 15:48:18 -0300

    hosting config

---
+++ firebase.config.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAyGEucoTAVUmVXXK92889yXleSyC8IOn0",
+  authDomain: "coder-site.firebaseapp.com",
+  projectId: "coder-site",
+  storageBucket: "coder-site.appspot.com",
+  messagingSenderId: "1098397068911",
+  appId: "1:1098397068911:web:2c04975878369e69a1a79a"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
